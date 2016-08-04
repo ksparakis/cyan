d23eaa5d936c6bf09df4de9e56277b94ada31eed
Konstantino Sparakis <ksparakis@gmail.com>
2016-08-04 16:46:15 -0400

    organized code

---
+++ src/index.js
@@ -0,0 +1,169 @@
+
+var DEBUG = 1; // 1 = console log messages appear, 0 = no console messages
+var https = 1; // 1 = https, 0 = http; for security you want https running
+
+
+
+// Setup of Express Server
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var server;
+if(https === 1)
+{
+  var credentials = {
+    // To generate and sign your own keys follow these steps in a terminal:
+    //  openssl genrsa -des3 -out server.enc.key 1024
+    //  openssl req -new -key server.enc.key -out server.csr
+    //  openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+    key  : fs.readFileSync('ssl/server.key'),
+    cert : fs.readFileSync('ssl/server.crt')
+  };
+  server = require('https').createServer(credentials, app);
+}
+else
+{
+  server = require('http').createServer( app);
+}
+var io = require('./')(server);
+var port = process.env.PORT || 3000;
+
+
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+//Global connection variabls
+var numDevices = 0;
+var numUsers = 0;
+
+
+/*
+*
+*  SOCKET IO connection handeling
+*
+*/
+io.on('connection', function (socket) {
+  var addedUser = false;
+  console.log("connection");
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('device/connect', function (device_id) {
+    console.log("device connected");
+
+    // we store the  device_id in the socket session for this client
+    socket.device_id = device_id;
+    ++numDevices;
+    socket.emit('registered', {
+      numDevices: numDevices
+  });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('device added', {
+      username: socket.device_id,
+    });
+
+  });
+
+
+// when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+    }
+  });
+
+
+
+/*
+*
+*     Socket controlls for user clients
+*
+*/
+
+ 
+//check the username to see if its valid
+  socket.on('login/check_username', function (username) {
+    if (addedUser) {
+      --numUsers;
+    }
+  });
+
+// logs user in and returns an access token
+  socket.on('login', function (username, password) {
+
+  });
+
+// disables user token
+  socket.on('logout', function (user_token) {
+
+  });
+
+
+//returns a list of deivces
+  socket.on('devices/get', function (user_token) {
+
+  });
+
+
+// creates a new device
+  socket.on('device/new', function (device_name, device_type) {
+
+  });
+
+  //returns a list of devices
+  socket.on('device/edit', function (user_token, device_id) {
+
+  });
+
+ //deletes the specified device
+  socket.on('device/delete', function (user_token, device_id) {
+
+  });
+
+//returns a json of all device data relevant to that device
+  socket.on('device/get/data', function (user_token) {
+
+  });
+
+   //turns a specified device on
+  socket.on('device/on', function (device_id) {
+    console.log("Recieved On command");
+    socket.broadcast.emit('on', {
+      device_id: socket.device_id,
+    });
+  });
+
+  //turns a specified device off
+  socket.on('device/off', function (device_id) {
+     socket.broadcast.emit('off', {
+      device_id: socket.device_id,
+    });
+  });
+
+
+/*
+*
+*     Socket controlls for devices
+*
+*/
+
+// logs user in and returns an access token
+  socket.on('device/post/data', function (device_id, data) {
+
+  });
+
+
+
+});
+
+
