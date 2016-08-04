5bbebec8cc956ea4c7e6d699621dd6a182344ff1
Konstantino Sparakis <ksparakis@gmail.com>
2016-08-04 17:35:36 -0400

    added license to code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+/*
+    Cyan, Home automation api and platform
+    Copyright (C) <2016>  <Konstantino Sparakis>
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//OPTIONS
 var DEBUG = 1; // 1 = console log messages appear, 0 = no console messages
 var https = 1; // 1 = https, 0 = http; for security you want https running
-
-
 
 // Setup of Express Server
 var fs = require('fs');
